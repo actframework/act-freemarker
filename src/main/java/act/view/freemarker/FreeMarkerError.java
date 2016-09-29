d213dc2db4bc7940d652f8a52fad203f4e2f37d9
Gelin Luo <greenlaw110@gmail.com>
2016-09-30 00:57:25 +1000

    WIP

--- src/main/java/act/view/freemarker/FreeMarkerError.java
+++ src/main/java/act/view/freemarker/FreeMarkerError.java
@@ -2,20 +2,18 @@ package act.view.freemarker;
 
 import act.Act;
 import act.app.SourceInfo;
-import act.view.TemplateError;
 import freemarker.core.ParseException;
-import freemarker.template.TemplateException;
 import org.osgl.util.E;
 
 import java.util.List;
 
-public class FreeMarkerError extends TemplateError {
+public class FreeMarkerError extends act.view.TemplateException {
 
     public FreeMarkerError(ParseException t) {
         super(t);
     }
 
-    public FreeMarkerError(TemplateException t) {
+    public FreeMarkerError(freemarker.template.TemplateException t) {
         super(t);
     }
 
@@ -23,8 +21,8 @@ public class FreeMarkerError extends TemplateError {
     protected void populateSourceInfo(Throwable t) {
         if (t instanceof ParseException) {
             templateInfo = new FreeMarkerSourceInfo((ParseException) t);
-        } else if (t instanceof TemplateException) {
-            templateInfo = new FreeMarkerSourceInfo((TemplateException) t);
+        } else if (t instanceof freemarker.template.TemplateException) {
+            templateInfo = new FreeMarkerSourceInfo((freemarker.template.TemplateException) t);
         } else {
             throw E.unexpected("Unknown exception type: %s", t.getClass());
         }
@@ -35,11 +33,7 @@ public class FreeMarkerError extends TemplateError {
         return getCauseOrThis().getMessage();
     }
 
-    private static class FreeMarkerSourceInfo implements SourceInfo {
-
-        private String fileName;
-        private List<String> lines;
-        private int lineNumber;
+    private static class FreeMarkerSourceInfo extends SourceInfo.Base {
 
         FreeMarkerSourceInfo(ParseException e) {
             lineNumber = e.getLineNumber();
@@ -47,30 +41,10 @@ public class FreeMarkerError extends TemplateError {
             lines = readTemplateSource(fileName);
         }
 
-        FreeMarkerSourceInfo(TemplateException e) {
+        FreeMarkerSourceInfo(freemarker.template.TemplateException e) {
             lineNumber = e.getLineNumber();
             fileName = e.getTemplateSourceName();
             lines = readTemplateSource(fileName);
-        }
-
-        @Override
-        public String fileName() {
-            return fileName;
-        }
-
-        @Override
-        public List<String> lines() {
-            return lines;
-        }
-
-        @Override
-        public Integer lineNumber() {
-            return lineNumber;
-        }
-
-        @Override
-        public boolean isSourceAvailable() {
-            return true;
         }
 
         private static List<String> readTemplateSource(String template) {
